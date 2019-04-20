3dfb204762740a32e228b1d2256ec2f096bc84c3
Fernando Azuaje <fjah17@gmail.com>
2019-04-19 22:11:03 -0400

    add setups to index and config relations in models

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,4 +1,17 @@
+const setupDatabse = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function config (config) {
+  const sequelize = setupDatabse(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
   const Agent = {}
   const Metric = {}
 
