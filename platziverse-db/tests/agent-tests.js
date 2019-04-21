677878205c756e48d992639b9cdb8a9f5ee49909
Fernando Azuaje <fjah17@gmail.com>
2019-04-21 13:15:31 -0400

    add proxiquire y sainon, creacion de un sandbox

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -1,17 +1,43 @@
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 let config = {
     loggin: function (){}
 }
 
+let MetricStub = {
+    belongsTo: sinon.spy()
+}
+
+let AgentStub = null
 let db = null
+let sandbox = null
 
 
 test.beforeEach(async () => {
-    const setupDatabase = require('../')
+    sandbox = sinon.createSandbox()
+    AgentStub = {
+        hasMany: sinon.spy()
+    }
+
+    const setupDatabase = proxyquire('../', {
+        './models/agent': () => AgentStub,
+        './models/metric': () => MetricStub
+    })
+    
     db = await setupDatabase(config)
+})
+
+test.afterEach( t  => {
+    sandbox && sandbox.restore()
 })
 
 test('Agent', t => {
     t.truthy(db.Agent, 'Agent service should exit')
+})
+
+test.serial('Setup', t => {
+    t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+    t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
 })
