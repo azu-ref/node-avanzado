f9e495411ce4c0f584c06e96121e484634acbf0a
Fernando Azuaje <fjah17@gmail.com>
2019-04-20 20:35:43 -0400

    configuracion para purebas con ava

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,8 +1,22 @@
 const setupDatabse = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
 
 module.exports = async function config (config) {
+  config = defaults( config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 10,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+
+
   const sequelize = setupDatabse(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
