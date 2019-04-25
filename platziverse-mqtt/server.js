db021368327296e46a566964ecab00b1a947d68a
Fernando Azuaje <fjah17@gmail.com>
2019-04-25 17:23:47 -0400

    creando servidor mqtt

---
+++ platziverse-mqtt/server.js
@@ -0,0 +1,45 @@
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+
+const backend = {
+    type: 'redis',
+    redis,
+    return_buffers: true 
+}
+
+const settings = {
+    port: 1883,
+    backend
+}
+
+const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+    debug(`Client connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+    debug(`Client disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+    debug(`Received: ${packet.topic}`)
+    debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', () => {
+    console.log(`${chalk.green('[platziverse-mqtt]')} is runnig` )
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError(err){
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+server.on('uncaughtException', handleFatalError)
+server.on('unhandledRejection', handleFatalError)
