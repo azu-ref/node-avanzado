24d2d06526a35ab49206ad2ab66278cd5474d0c7
Fernando Azuaje <fjah17@gmail.com>
2019-05-01 14:56:29 -0400

    terminando la configuracion del servidor mqtt

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,51 +4,146 @@ const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziverse-db')
 
+const { parsePayload } = require('./utils')
+
 const backend = {
-    type: 'redis',
-    redis,
-    return_buffers: true 
+  type: 'redis',
+  redis,
+  return_buffers: true
 }
 
 const settings = {
-    port: 1883,
-    backend
+  port: 1883,
+  backend
 }
 
 const config = require('../platziverse-db/config-db')(false)
 
 const server = new mosca.Server(settings)
+const clients = new Map()
 
 let Agent, Metric
 
 server.on('clientConnected', client => {
-    debug(`Client connected: ${client.id}`)
+  debug(`Client connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
-server.on('clientDisconnected', client => {
-    debug(`Client disconnected: ${client.id}`)
+server.on('clientDisconnected', async (client) => {
+  debug(`Client disconnected: ${client.id}`)
+  const agent = clients.get(client.id)
+
+  if (agent) { // mark agent as disconnected
+    agent.connected = false
+
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (e) {
+      return handleError(e)
+    }
+
+    // Delete agent for from clients list
+    clients.delete(client.id)
+
+    server.publish({
+      topic: 'agent/disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid
+        }
+      })
+    })
+
+    debug(`Client (${client.id}) associated to Agent (${Agent.uuid}) marked as disconnected`)
+  }
 })
 
-server.on('published', (packet, client) => {
-    debug(`Received: ${packet.topic}`)
-    debug(`Payload: ${packet.payload}`)
+server.on('published', async (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // notify agent is connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            agent: {
+              uuid: agent.uuid,
+              name: agent.name,
+              hostname: agent.hostname,
+              pid: agent.pid,
+              connected: agent.connected
+            }
+          })
+        }
+
+        // store metrics
+        const savedMetricPromises = payload.metrics.map( async (metric) => {
+          let m
+
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (e) {
+            return handleError(e)
+          }
+
+          debug(`Metric ${m.id} saved on agent ${agent.uuid}`)
+        })
+
+        try{
+            await Promise.all(savedMetricPromises)
+        }catch(e){
+            handleError(e)
+        }
+      }
+      break
+  }
+
+  debug(`Payload: ${packet.payload}`)
 })
 
 server.on('ready', async () => {
-    const services = await db(config).catch(handleFatalError)
+  const services = await db(config).catch(handleFatalError)
 
-    Agent = services.Agent
-    Metric = services.Metric
-    
-    console.log(`${chalk.green('[platziverse-mqtt]')} is runnig` )
+  Agent = services.Agent
+  Metric = services.Metric
+
+  console.log(`${chalk.green('[platziverse-mqtt]')} is runnig`)
 })
 
 server.on('error', handleFatalError)
 
-function handleFatalError(err){
-    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
-    console.error(err.stack)
-    process.exit(1)
+function handleFatalError (err) {
+  console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[Error]')} ${err.message}`)
+  console.error(err.stack)
 }
 
 server.on('uncaughtException', handleFatalError)
