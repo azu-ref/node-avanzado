f491763c07113d43bef12e697e4ce3a419416038
Fernando Azuaje <fjah17@gmail.com>
2019-05-02 23:49:03 -0400

    todo funcionando

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,23 +1,71 @@
 const eventEmitter = require('events')
+const debug = require('debug')('platziverse:agent')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
+const uuid = require('uuid')
+
+const {parsePayload} = require('./utils')
+
+const options = {
+    name: 'untitled',
+    username: 'platzi',
+    interval: 5000,
+    mqtt:{
+        host: 'mqtt://localhost'
+    }
+}
 
 class PlatziverseAgent extends eventEmitter {
     constructor(opts){
         super()
-        this._options = opts
+        this._options = defaults(opts, options)
         this._started = false
         this._timer = null
+        this._client = null
+        this._agentId = null
     }
 
     connect(){
         if(!this._started){
-            this._started = true
-            
-            this.emit('connected')
-
             const opt = this._options
-            this._timer = setInterval( () => {
-                this.emit('agent/message', 'This is a Message')
-            }, opt.interval)
+            this._client = mqtt.connect(opts.mqtt.host)
+
+            this._started = true
+
+            this._client.subscribe('agent/message')
+            this._client.subscribe('agent/connected')
+            this._client.subscribe('agent/disconnected')
+
+            this._client.on('connect', () => {
+                this._agentId = uuid.v4()
+                this.emit('connected', this._agentId)
+
+                this._timer = setInterval( () => {
+                    this.emit('agent/message', 'This is a Message')
+                }, opt.interval)
+            })
+
+            this._client.on('message', (topic, payload) => {
+                payload = parsePayload(payload)
+
+                let broadcast = false
+                switch (topic) {
+                    case 'agent/connected':
+                    case 'agent/disconnected':
+                    case 'agent/message':
+                        broadcast = payload && payload.agent && payload.agent.uuid != this._agentId
+                    break
+                }
+
+                if(broadcast){
+                    this.emit(topic, payload)
+                }
+
+            })
+
+            this._client.on('error', () => this.disconnect())
+            
+            
         }
     }
 
