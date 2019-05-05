7f63e5f27499f70356d36b59842a8d1ed2511846
Fernando Azuaje <fjah17@gmail.com>
2019-05-05 01:23:04 -0400

    configurando rutas, errores y test

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -1,12 +1,40 @@
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
+const debug = require('debug')('PLatziverse:api:server')
 
+const api = require('./api')
+
+const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
 
-const port = process.env.PORT || 3000
+app.use('/api', api)
 
-server.listen(port, () => {
-    console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
+//Express error handler
+app.use( (err, req, res, next) => {
+    debug(`Error ${err.message}`)
+
+    if(err.message.match(/not found/)){
+        return res.status(404).send({ error: err.message })
+    }
+
+    res.status(500).send({ error: err.message})
 })
+
+function handleFatalError(err){
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+if(!module.parent){
+    process.on('uncaughtException', handleFatalError)
+    process.on('unhandledException', handleFatalError)
+
+    server.listen(port, () => {
+    console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
+    })
+}
+
+module.exports = server
