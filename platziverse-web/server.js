558327baaacdae35ba050adc6ccd4de16f20a5c6
Fernando Azuaje <fjah17@gmail.com>
2019-05-08 23:03:58 -0400

    pruebas con socket.io

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -1,14 +1,31 @@
 const http = require('http')
 const path = require('path')
 const express = require('express')
-const debug = require('debug')('Platziverse:web')
+const debug = require('debug')('platziverse:web')
+const socketio = require('socket.io')
 const chalk = require('chalk')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+//sockect.io
+
+io.on('connect', socket => {
+    debug(`Connect ${socket.id}`)
+
+    socket.on('agent/message', payload => {
+        console.log(payload)
+    })
+
+    setInterval(()=>{
+        socket.emit('agent/message', {agent: 'xxx-yyy'})
+
+    }, 5000)
+})
 
 function handleFatalError (err) {
     console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
