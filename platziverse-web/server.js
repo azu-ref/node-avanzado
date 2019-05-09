a3d06a341e8f5d7749dbc6cd112cd94b6238d102
Fernando Azuaje <fjah17@gmail.com>
2019-05-08 21:50:29 -0400

    sirviendo stactic files

---
+++ platziverse-web/server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const path = require('path')
+const express = require('express')
+const debug = require('debug')('Platziverse:web')
+const chalk = require('chalk')
+
+const port = process.env.PORT || 8080
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+function handleFatalError (err) {
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+  }
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledException', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[Platziverse-web]')} server listening on port ${port}`)
+})
