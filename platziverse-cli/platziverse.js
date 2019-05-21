9c4cd144e17fbf789e5dc8eb72f15417a1191ec3
Fernando Azuaje <fjah17@gmail.com>
2019-05-21 02:29:18 -0400

    implementando el modulo args

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
-const minimist = require('minimist')
+// const minimist = require('minimist')
 
-console.log('Hello Platziverse!')
+// console.log('Hello Platziverse!')
 
-const args = minimist(process.argv)
+// const args = minimist(process.argv)
 
-console.log(args.name)
-console.log(args.host)
+// console.log(args.name)
+// console.log(args.host)
+
+const args = require('args')
+
+args
+  .option('port', 'The port on which the app will be running', 3000)
+  .option('reload', 'Enable/disable livereloading')
+  .command('serve', 'Serve your static site', ['s'])
+ 
+const flags = args.parse(process.argv)
